eff0b367ff3bc2fd0099d2b8c20c6e05a8608d72
Tanveer432 <78549858+Tanveer432@users.noreply.github.com>
2023-04-07 02:31:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const methodOverride=require('method-override');
 const passport=require('passport');
 const LocalStrategy=require('passport-local');
 const { ObjectId } = require('mongodb');
-const routes = require('./routes/route');
+// const routes = require('./routes/route');
 
 mongoose.connect('mongodb://localhost:27017/srms',{
     useNewUrlParser:true,
@@ -61,7 +61,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use('/', routes);
+// app.use('/', routes);
 
 app.get('/',(req, res) => {
     res.render('home');
@@ -161,12 +161,100 @@ app.post('/studentLogin', passport.authenticate('local', { failureRedirect: '/st
     // If authentication is successful, redirect to the teacher's dashboard page with the teacher's ID in the URL
     res.redirect('/studentResult/' + req.user._id);
   });
-app.get('/logout', function(req, res, next) {
+app.get('/add_faculty',async(req,res) => {
+     res.render('add_faculty');
+});
+app.post('/add_faculty',function(req, res, next) => {
+     const { email } = req.body;
+     const count = await db.collection('staff').countDocuments({ email: email });
+    if (count !== 0) {
+      req.flash('error', 'Staff with that email already exists');
+      res.redirect('/add_faculty');
+    } else {
+      const {
+        dob,
+        name,
+        subject,
+        contact,
+      } = req.body;
+  
+      if (contact.length > 11) {
+        req.flash('error', 'Enter a valid phone number');
+        return res.redirect('/add_faculty');
+      }
+  
+      const password = dob.toString().split('-').join('');
+      const hashedPassword = await bcrypt.hash(password, 8);
+  
+      const newStaff = {
+        st_id: uuidv4(),
+        st_name: name,
+        dob: dob,
+        subject: subject,
+        email: email,
+        contact: contact,
+        password: hashedPassword,
+      };
+       await db.collection('staff').insertOne(newStaff);
+      req.flash('success_msg', 'Staff added successfully');
+      res.redirect('/manage_faculty');
+    }
+});
+app.get('/manage_faculty',async(req,res) => {
+    const collection = db.collection('staff');
+    const results = await zeroParamPromise(collection);
+    res.render('manage_faculty', {data: results});
+});
+app.get('/edit_faculty',async(req,res) => {
+    const staffEmail = req.params.id;
+     const staffData = await db().collection('staff').findOne({ email: staffEmail });
+    res.render('/manage_faculty', {staffData: staffData,});
+});
+app.post('/edit_faculty',async(req,res,next) => {
+    const {
+    old_email,
+    email,
+    dob,
+    name,
+    subject,
+    contact,
+  } = req.body;
+
+  const password = dob.toString().split('-').join('');
+  const hashedPassword = await bcrypt.hash(password, 8);
+
+  await db().collection('staff').updateOne(
+    { email: old_email },
+    {
+      $set: {
+        st_name: name,
+        dob: dob,
+        subject: subject,
+        email: email,
+        contact: contact,
+        password: hashedPassword,
+      },
+    }
+  );
+  req.flash('success_msg', 'Data modified successfully');
+  res.redirect('/manage_faculty');
+});
+app.get('/manage_student',async(req,res,next) => {
+  const results = await Student.find({});
+  res.render('/manage_student', {data: results});
+});
+app.get('/add_students',async(req,res,next) => {
+   res.render('add_students');
+});
+app.post('/add_students',async(req,res,next) => {
+  res.redirect('manage_student');
+});
+app.get('/logout', async(req, res, next) {
     req.logout(function(err) {
       if (err) { return next(err); }
       res.redirect('/');
     });
-    
+   
  });
 // app.get('/studentResult/:id', async (req, res)=>{
 //     const studId=req.params.id;
